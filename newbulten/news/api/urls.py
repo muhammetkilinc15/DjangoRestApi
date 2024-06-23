7a64cd5807a445358195722b9d30c65ec5fa4edf
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-23 18:29:51 +0300

    Added article-details url

--- newbulten/news/api/urls.py
+++ newbulten/news/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from news.api import views
 urlpatterns = [
     path('articles/',views.Article_list_create_api_view,name="ListArticles"),
-    path('articles/<int>:articleid',views.GetArticle,name="GetArticle")
+    path('articles/<int:pk>',views.article_detail_api_view,name="Article-detail")
 ]
